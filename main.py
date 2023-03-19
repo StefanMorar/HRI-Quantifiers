e180a77080637bf4f0c5db790076971d0afe524e
Stefan Morar <stefan.morar30@gmail.com>
2023-03-19 11:32:41 +0200

    Remove first version

--- main.py
+++
@@ -1,8 +0,0 @@
-from nltk import DiscourseTester, CfgReadingCommand
-
-dt = DiscourseTester(['Robot move a box',
-                      'Robot move a red box',
-                      'Robot move all boxes'],
-                     CfgReadingCommand('./grammar/discourse.fcfg'))
-dt.sentences()
-dt.readings()
