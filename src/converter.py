26e873ac1316c7997d06edcb299687c1c5a7477e
Stefan Morar <stefan.morar30@gmail.com>
2023-03-26 21:16:17 +0300

    Add implementation for "Most () are ()"

--- src/converter.py
+++ src/converter.py
@@ -1,4 +1,5 @@
 import re
+from itertools import combinations
 
 import inflect
 import nltk
@@ -21,17 +22,36 @@ def get_tag_string(tagged_tokens):
     return ''.join('<{}>'.format(tagged_token[1]) for tagged_token in tagged_tokens)
 
 
-def generate_exists_expression(tagged_tokens):
-    predicates = [get_predicate(t) for t in tagged_tokens]
-    no_predicates = len(predicates)
+def generate_exists_expression2(positive_predicate_tagged_tokens, negative_predicate_tagged_tokens, no_variables):
+    signs = ['+'] * len(positive_predicate_tagged_tokens) + ['-'] * len(negative_predicate_tagged_tokens)
+    predicates = [get_predicate(t) for t in positive_predicate_tagged_tokens + negative_predicate_tagged_tokens]
+
+    sign_predicates = list(zip(predicates, signs))
+    no_predicates = len(sign_predicates) * no_variables
     if no_predicates == 0:
         return ''
-    elif no_predicates == 1:
-        return 'exists x ({}(x)).'.format(predicates[0])
     else:
-        inner_expression = '{}(x) '.format(predicates[0]) + ' '.join(
-            ['& {}(x)'.format(predicate) for predicate in predicates[1:]])
-        return 'exists x ({}).'.format(inner_expression)
+        inner_expression = ''
+        for iterator in range(no_variables):
+            partial_inner_expression = ' & '.join(
+                [('' if sign_predicate[1] == '+' else '-') + '{}(x{})'.format(sign_predicate[0], iterator) for
+                 sign_predicate in
+                 sign_predicates])
+            if iterator == 0:
+                inner_expression = partial_inner_expression
+            else:
+                inner_expression = ' & '.join([inner_expression, partial_inner_expression])
+
+        variables = ['x{}'.format(current_variable) for current_variable in range(no_variables)]
+        if no_variables > 1:
+            variable_combinations = list(combinations(variables, 2))
+            c = ' & '.join(
+                '{} != {}'.format(variable_combination[0], variable_combination[1]) for variable_combination in
+                variable_combinations)
+            inner_expression = '{} & {}'.format(inner_expression, c)
+
+        outer_expression = 'exists ' + ' exists '.join(variables)
+        return '{} ({}).'.format(outer_expression, inner_expression)
 
 
 def validate_noun_phrase(tagged_tokens):
@@ -46,24 +66,41 @@ def generate_integer_quantifier(tagged_tokens):
         return 3
 
 
-def generate_predicate_comparison_expression(sentence):
-    regex_pattern = r'There are (.*) as many (.*) than (.*)'
-    match = re.match(regex_pattern, sentence)
-    if match:
-        quantifier = nltk.pos_tag(nltk.word_tokenize(match.group(1)))
-        noun_phrase1 = nltk.pos_tag(nltk.word_tokenize(match.group(2)))
-        print(noun_phrase1)
-        noun_phrase2 = nltk.pos_tag(nltk.word_tokenize(match.group(3)))
-        if (not validate_noun_phrase(noun_phrase1)) | (not validate_noun_phrase(noun_phrase2)):
-            return None
-
-        return '|{}| == {} * |{}|'.format(generate_exists_expression(noun_phrase1),
-                                          generate_integer_quantifier(quantifier),
-                                          generate_exists_expression(noun_phrase2))
-    else:
+def generate_as_many_than_expression(match):
+    quantifier = nltk.pos_tag(nltk.word_tokenize(match.group(1)))
+    noun_phrase1 = nltk.pos_tag(nltk.word_tokenize(match.group(2)))
+    noun_phrase2 = nltk.pos_tag(nltk.word_tokenize(match.group(3)))
+    if (not validate_noun_phrase(noun_phrase1)) | (not validate_noun_phrase(noun_phrase2)):
         return None
 
+    return '|{}| == {} * |{}|'.format(generate_exists_expression2(noun_phrase1, [], 1),
+                                      generate_integer_quantifier(quantifier),
+                                      generate_exists_expression2(noun_phrase2, [], 1))
 
-expression = generate_predicate_comparison_expression('There are twice as many boxes than tools')
+
+def generate_most_expression(match):
+    noun_phrase1 = nltk.pos_tag(nltk.word_tokenize(match.group(1)))
+    noun_phrase2 = nltk.pos_tag(nltk.word_tokenize(match.group(2)))
+    if (not validate_noun_phrase(noun_phrase1)) | (not validate_noun_phrase(noun_phrase2)):
+        return None
+    return '|{}| > |{}|'.format(generate_exists_expression2(noun_phrase1 + noun_phrase2, [], 1),
+                                generate_exists_expression2(noun_phrase1, noun_phrase2, 1))
+
+
+def generate_expression(sentence):
+    match = re.match(r'There are (.*) as many (.*) than (.*)', sentence)
+    if match:
+        return generate_as_many_than_expression(match)
+    match = re.match(r'Most (.*) are (.*)', sentence)
+    if match:
+        return generate_most_expression(match)
+    return None
+
+
+expression = generate_expression('There are twice as many boxes than tools')
+print(expression)
+print(evaluate_fol_cardinality_expression(expression))
+
+expression = generate_expression('Most objects are boxes')
 print(expression)
 print(evaluate_fol_cardinality_expression(expression))
